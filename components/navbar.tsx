f253ebe24fefe583d705d246be835c5564090668
Oludowole Olumide <xeonncodes@gmail.com>
2024-08-31 22:44:32 +0100

    updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,37 @@
 import { siteConfig } from "@/config/site";
+import { getSession } from "@/util/actions";
 import Link from "next/link";
 import React from "react";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getSession();
+
   return (
     <div className="p-4">
-      <div>
-        <Link
-          className="text-lg"
-          href={`${siteConfig.pathLinks.trainingDashboard}`}
-        >
-          Dashboard
-        </Link>
-      </div>
-      <div>
-        <Link className="text-lg" href={`${siteConfig.pathLinks.signin}`}>
-          Sign In
-        </Link>
-      </div>
-      <div>
-        <Link className="text-lg" href={`${siteConfig.pathLinks.register}`}>
-          Register
-        </Link>
-      </div>
+      {session.isLoggedIn && (
+        <div>
+          <Link
+            className="text-lg"
+            href={`${siteConfig.pathLinks.trainingDashboard}`}
+          >
+            Dashboard
+          </Link>
+        </div>
+      )}
+      {!session.isLoggedIn && (
+        <>
+          <div>
+            <Link className="text-lg" href={`${siteConfig.pathLinks.signin}`}>
+              Sign In
+            </Link>
+          </div>
+          <div>
+            <Link className="text-lg" href={`${siteConfig.pathLinks.register}`}>
+              Register
+            </Link>
+          </div>
+        </>
+      )}
     </div>
   );
 }
